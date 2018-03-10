62283264e1503a718ce972247de89f41dfa4a4e3
cpoIT <katherynejf@gmail.com>
2018-03-10 08:15:42 -0500

    Update FinderFunctionBonus.js

--- FinderFunctionBonus.js
+++ FinderFunctionBonus.js
@@ -1,15 +1,17 @@
-function finderFunctionBonus(anArray, testFunction) {
-  newArray = [];
-  for (var i = 0; i < anArray.length; i++) {
-    if (testFunction(anArray[i]) === true) {
-      newArray.push(anArray[i])
-      result = newArray
-    } else {
-      result = anArray.indexOf()
-    }
-  }
-    return result
-}
-
-
+function isEven (num) {
+  return num % 2 === 0 ? true: false ;
+ }
+ 
+ numbers = [1, 2, 3];
+ 
+ function finderFunctionBonus(anArray, testFunction) {
+   newArray = [];
+   for (let i = 0; i < anArray.length; i++) {
+     if (testFunction(anArray[i]) === true) {
+       newArray.push(anArray[i])
+     }
+   }
+     return newArray.length > 0 ? newArray: -1;
+ }
+ 
 finderFunctionBonus(numbers, isEven);
